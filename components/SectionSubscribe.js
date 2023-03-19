52da33f12da3b56a47807c11d145fd443d6c9bf1
danggro <anangdwin55@gmail.com>
2023-03-19 14:35:56 +0700

    perbaikan Subscribe Section

--- components/SectionSubscribe.js
+++ components/SectionSubscribe.js
@@ -2,9 +2,9 @@ import Button from "./Button";
 
 export default function SectionSubscribe() {
   return (
-    <div className="ml-[135px] min-w-screen bg-orange mt-[128px] text-white py-[60px] mb-[130px]">
+    <div className="ml-[135px] min-w-screen bg-orange mt-[128px] text-white py-[70px] mb-[130px]">
       <div className="w-7/12 mx-auto">
-        <h1 className="text-[48px] text-center leading-[75px] font-medium">
+        <h1 className="text-[48px] text-center leading-[75px] font-medium px-10">
           Subscribe to get a discount of 30%
         </h1>
         <div className="p-4 bg-white text-secondary text-[16px] flex mt-16">
@@ -13,7 +13,7 @@ export default function SectionSubscribe() {
             placeholder="Enter your email address"
             className="w-full px-5"
           />
-          <Button className="px-10 py-6 ">Subscribe</Button>
+          <Button className="px-10 py-8 ">Subscribe</Button>
         </div>
       </div>
     </div>
